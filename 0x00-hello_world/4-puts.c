bc5cb71433e2d94e637ebe814b68f05aaf033b86
’NoussaNoussa’ <Noussaibahassan2@gmail.com>
2023-06-19 15:18:31 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle,
- *  followed by a new line."
- * Return: Always 0 (Success)
+ * main - 4-puts.c
+ * description - Write a C program that prints exactly
+ *
+  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-
         puts("\"Programming is like building a multilingual puzzle");
 
         return (0);
 }
+
 
