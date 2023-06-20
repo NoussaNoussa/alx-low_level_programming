aa13819dc39fd4d51c1ee96e060fd7a8be489135
’NoussaNoussa’ <Noussaibahassan2@gmail.com>
2023-06-19 18:03:45 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - 5-printf.c
- * description - Write a C program that prints exactly with proper grammar,
- *
- * Return: Always 0 (Success)
+ * main - A program that print a line using the printf f 
+ unction 
+ * Return: 0 (success)
  */
+
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-
-        return (0);
-
+	printf("with proper grammer, but the outcome is 
+a piece of art,\n")
+		return(0);
+}
